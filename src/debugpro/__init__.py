bf6c39f20c7abcb26a7641a33e05fea0eaa9a81c
Travis <travisreid0901@gmail.com>
2025-04-14 00:23:30 -0500

    Explain the __init.py__ expose

--- src/debugpro/__init__.py
+++ src/debugpro/__init__.py
@@ -1,1 +1,2 @@
-from .excepthook import install_custom_excepthook
+# This exposes the function from debugpro rather than requiring the user to write "debugpro.excepthook"
+from .excepthook import install_custom_excepthook 
