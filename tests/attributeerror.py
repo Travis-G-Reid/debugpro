9de462ee42d124c5c4c99a9bb0540e9c97371b31
Travis <travisreid0901@gmail.com>
2025-05-18 14:07:43 -0500

    make attribute error actually test attribute error

--- tests/attributeerror.py
+++ tests/attributeerror.py
@@ -2,7 +2,7 @@ from debugpro import install_custom_excepthook
 
 def test_AttributeError():
     my_dict = {'a': 1, 'b': 2, 'c': 3}
-    my_dict[5]
+    my_dict.grab_item('a')  # This will raise an AttributeError
 
 if __name__ == "__main__":
     install_custom_excepthook()
